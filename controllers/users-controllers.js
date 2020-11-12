c26c151c1294bceee4f8dacba722cae1909e6e74
JulianaAraujo <juliana.gvk@gmail.com>
2020-11-12 19:51:23 -0300

    criado function cadastro de usuario

--- controllers/users-controllers.js
+++ controllers/users-controllers.js
@@ -4,8 +4,57 @@ const jwt = require('jsonwebtoken')
 const HttpError = require('../models/http-error')
 const User = require('../models/user')
 
-const signupUser = (req, res) => {
-  
-}
+const signupUser = async (req, res, next) => {
+    const { name, email, password } = req.body
 
-module.exports = signupUser;
+    let existingUser;
+
+    try {
+        existingUser = await User.findOne({ email })
+    } catch(error) {
+        const err1 = new HttpError('Erro para achar o usuario', 500)
+        return next(err1)
+    }
+
+    if (existingUser) {
+        const err = new HttpError('Usuario cadastrado no banco de dados', 422)
+        return next(err)
+    }
+      let hashedPassword;
+
+      try{
+        hashedPassword = await bcrypt.hash(password, 12)
+      } catch (error){
+        const err = new HttpError('Não foi possivel criar o usuario', 500)
+        return next(err)
+      }
+
+      const createdUser = new User ({
+        name,
+        email,
+        password : hashedPassword
+      });
+
+      try {
+        await createdUser.save()
+      } catch (error) {
+        const err = new HttpError('Não foi possivel salvar o usuario', 500)
+        return next(err)
+      }
+
+      let token;
+        try {
+          token = jwt.sign (
+            {userId: createdUser.id, email: createdUser.email },
+            'kjsdkjfhkjh',
+            {expiresIn: '1h'}
+            )
+        } catch (error) {
+          const err = new HttpError('Não foi possivel criar token de acesso', 500)
+        return next(err)
+        }
+      res.status(201).json({user: createdUser.id, email: createdUser.email, token: token})
+  }
+
+
+exports.signupUser = signupUser
--- routes/blog-users.js
+++ routes/blog-users.js
@@ -2,12 +2,10 @@ const express = require('express')
 const router = express.Router();
 const userController = require('../controllers/users-controllers')
 
-router.get('/test', () => {
-  //
-}, (req, res) => {
+router.get('/',  (req, res) => {
   res.json({users: 'users ok'})
 })
 
-router.get('/signup', userController.signupUser)
+router.post('/signup', userController.signupUser)
 
 module.exports = router;
